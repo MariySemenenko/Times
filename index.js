9ad5cd2f8adb5f3fe0a8a6375ee6ed5c2eaa470f
MariySemenenko <mashyni92@gmail.com>
2023-04-06 14:37:53 +0300

    result

--- index.js
+++ index.js
@@ -16,39 +16,104 @@
 // Нам потрібно організувати наш скрипт таким чином, щоб операції які виконуються
 // довше розпочати виконувати спочатку, щоб за час очікування інші операції були виконані.
 //
+// setTimeout(() => {
+//     console.log('pasta');
+// }, 8000);
+
+// setTimeout(() => {
+//     console.log('kakao');
+// }, 500);
+
+// setTimeout(() => {
+//     console.log('woter');
+// }, 0);
+
+// setTimeout(() => {
+//    console.log('beard');
+// }, 0);
+
+// setTimeout(() => {
+// console.log('vino');
+// }, 500);
+
 //
 //
 // 2. Напишемо невеликий скрипт за допомогою якого будемо виводити
 // кількість днів до великодня 2023. ( 2023-04-16 )
 //
+// const currentDate = new Date();//поточний час
+// const easterDate = new Date('2023-04-16');
+// console.log({currentDate, easterDate});
+// const currentDateNumber = currentDate.getDate();//дістаємо порядковий номер дня
+// const eastertDateNumber = easterDate.getDate();
+// console.log({currentDateNumber, eastertDateNumber});
+// const daysTillEaster = eastertDateNumber - currentDateNumber;
+// const result = document.getElementById('days-elem');//стукаю в html по id
+// result.innerHTML = daysTillEaster;//виводю на екран
+
+//якщо днів більше місяця
+//another way
+// const date1 = new Date('1/13/2010');
+// const date2 = new Date('12/15/2010');
+// const diffTame = Math.abs(date2 - date1);// тут різниця в мілісекундах
+// const diffDays = Math.ceil(diffTame / (1000 * 60 *60 * 24));//(1сек, 60сек, 60хв, 24 дні)
+// console.log(diffTame + ' milliseconds');
+// console.log(diffDays + ' days');
 //
 //
 // 3. Будемо писати невеличкий годинник на базі тої розмітки що ми маємо.
 // Використовуватимемо інтервал для зміни положення стрілок та клас Date для роботи з часом.
-// hr_rotation = 30 * hr + min / 2; // converting current time
-// min_rotation = 6 * min;
-// sec_rotation = 6 * sec;
-// hour.style.transform = `rotate(${hr_rotation}deg)`;
-// minute.style.transform = `rotate(${min_rotation}deg)`;
-// second.style.transform = `rotate(${sec_rotation}deg)`;
-//
+setInterval(() =>
+{
+    const currentDate = new Date();
+    const min = currentDate.getMinutes();
+    const hr = currentDate.getHours();
+    const sec = currentDate.getSeconds();
+    
+    const hr_rotation = 30 * hr + min / 2; // converting current time
+    const min_rotation = 6 * min;
+    const sec_rotation = 6 * sec;
+    hour.style.transform = `rotate(${hr_rotation}deg)`;
+    minute.style.transform = `rotate(${min_rotation}deg)`;
+    second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
+
+//рухаємо стрілку назад
+
+// setInterval(() =>
+// {
+//     const currentDate = new Date();
+//     const min = currentDate.getMinutes();
+//     const hr = currentDate.getHours();
+//     const sec = currentDate.getSeconds();
+    
+//     const hr_rotation = -(30 * hr + min / 2); // converting current time
+//     const min_rotation = -(6 * min);
+//     const sec_rotation = -(6 * sec);
+//     hour.style.transform = `rotate(${hr_rotation}deg)`;
+//     minute.style.transform = `rotate(${min_rotation}deg)`;
+//     second.style.transform = `rotate(${sec_rotation}deg)`;
+// }, 1000);
+
 //
 //
 // 4. Будемо писати скрипт який перевірятиме чи користувач досі на сторінці
 // якщо користувач не наживає нікуди протягом 10-секунд - будемо виводити
 // confirm який перевірятиме чи користувач досі знаходиться на сторінці.
-// const message = 'Are you still here';
-// const timeoutInMilliseconds = 60000;
-// const timeoutHandler = () => {
-//     if (confirm(message)) {
-//         window.clearTimeout(timeoutHandler);
-//         window.setTimeout(timeoutHandler, timeoutInMilliseconds);
-//     } else {
-//         window.clearTimeout(timeoutHandler);
-//         window.close();
-//     }
-// }
-// window.setTimeout(timeoutHandler, timeoutInMilliseconds);
-// document.addEventListener('click', () => {
-//     window.clearTimeout(timeoutHandler);
-// });
+// 
+
+const message = 'Are you still here';
+const timeoutInMilliseconds = 10000;
+const timeoutHandler = () => {
+    if (confirm(message)) { //confirm вбудований метод в браузері щоб щось перевірити
+        window.clearTimeout(timeoutHandler);
+        window.setTimeout(timeoutHandler, timeoutInMilliseconds);
+    } else {
+        window.clearTimeout(timeoutHandler);
+        window.close();
+    }
+}
+window.setTimeout(timeoutHandler, timeoutInMilliseconds);
+document.addEventListener('click', () => {
+    window.clearTimeout(timeoutHandler);
+});
